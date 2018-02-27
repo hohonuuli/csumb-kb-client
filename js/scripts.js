610315d14a57533d658595cce206bbb9b994dd03
Meya Gorbea <meyagorbea@yahoo.com>
2018-02-27 14:53:50 -0800

    simple create concept form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(function() {
   console.log("Hello jQuery");
+  $.get("http://localhost:8083/kb/v1/concept/", function(data, status){
+    console.log(status);
+    console.log(JSON.stringify(data));
+  });
+
+  $('label.tree-toggle').click(function () {
+        $(this).parent().children('ul.tree').toggle(250); //show and hide children
+        $(this).children('i').toggleClass('glyphicon glyphicon-chevron-right');  //switch icons
+        $(this).children('i').toggleClass('glyphicon glyphicon-chevron-down');
+    });
+
 });
