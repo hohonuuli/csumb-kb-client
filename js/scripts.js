739ae13220d9d90374cc2da380753db1e4f2206e
Tomas Hernandez <tohernandez@csumb.edu>
2018-03-02 21:41:33 -0800

    Added search functionality, displaying JSON to content of page, minor CSS changes and comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 var treeData = null;
+
 $(function() {
+  //When document is loaded
 
   //Gets all concepts
   $.get("http://localhost:8083/kb/v1/concept/", function(data, status){
     // console.log(JSON.stringify(data));
   });
-  $.get("http://localhost:8083/kb/v1/phylogeny/down/root", function(data, status){
+
+  $.get("http://localhost:8083/kb/v1/concept/object", function(data, status){
+    $("#objectConcept").html(JSON.stringify(data));
+  });
+
+  //Appends first level of tree
+  $.get("http://localhost:8083/kb/v1/phylogeny/down/object", function(data, status){
     treeData = data;
     $.each(treeData.children, function(k, v){
       if(!v.children){
@@ -17,12 +25,39 @@ $(function() {
   });
 });
 
+//When search button is clicked
+$("#searchButton").click(function(){
+  $.ajax({
+    url: "http://localhost:8083/kb/v1/concept/" + $("#searchInput").val(),
+    data: {},
+    type: 'get',
+    error: function(XMLHttpRequest, errorThrown){
+      $("#alertError").html("Error: ");
+      $("#alertText").html("Concept not found");
+      $(".alert").show();
+      $(".alert").delay(3200).fadeOut(300);
+    },
+    success: function(data){
+      var alternateNameString = "";
+      if(data.alternateNames){
+        $.each(data.alternateNames, function(k, v){
+          alternateNameString += (v + " ");
+        });
+      }
+      $(".sub-header").html(data.name + " ( " + alternateNameString + ")");
+      $("#objectConcept").html(JSON.stringify(data));
+    }
+  });
+});
+
+//Toggles glyphicon and expands tree on click
 $('i.tree-toggler').click(function () {
     $(this).parent().parent().children('ul.tree').toggle(250); //show and hide children
     $(this).toggleClass('glyphicon glyphicon-chevron-right');  //switch icons
     $(this).toggleClass('glyphicon glyphicon-chevron-down');
 });
 
+//Gets concept when name on tree view is clicked
 $('#view-toggler').click(function () {
     var conceptName = ($(this).html().toLowerCase()).replace(/\s/g, '%20');
     $.get("http://localhost:8083/kb/v1/concept/" + conceptName, function(data, status){
