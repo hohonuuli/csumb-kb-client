a047fd2f1b8f41aa2004d8d5536a02eabb6c15a0
Meya Gorbea <meyagorbea@yahoo.com>
2018-03-05 09:39:33 -0800

    post concept name

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,30 @@ $(function() {
     console.log(JSON.stringify(data));
   });
 
+  $("#post-btn").click(function(){
+      var concept = {
+          name: $("#c-name").val(),
+          //eventually will include author and synonym
+          // author:$("#c-author").val(),
+          // synonym:$("#c-synonym").val()
+      }
+
+      var name = $("#c-name").val()
+      $.ajax({
+          url: 'http://localhost:4567/createConcept/' + name,
+          type: 'post',
+          dataType: 'json',
+          success: function (data) {
+              console.log(data.msg)
+          },
+          error: function(status,error){
+              console.log(error.msg)
+          },
+          data: concept
+      });
+
+  });
+
   $('label.tree-toggle').click(function () {
         $(this).parent().children('ul.tree').toggle(250); //show and hide children
         $(this).children('i').toggleClass('glyphicon glyphicon-chevron-right');  //switch icons
