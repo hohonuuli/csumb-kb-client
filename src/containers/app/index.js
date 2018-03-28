d82df66bd215d2e2130bbab50640a56ccca24748
Tomas Hernandez <tohernandez@csumb.edu>
2018-03-28 15:27:51 -0700

    Auth checkpoint, have reducers + actions

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 import Header from '../../components/header';
 import Dashboard from '../dashboard';
 import Login from '../login';
 import Profile from '../profile';
 
-const App = () => (
-  <div>
-    <header>
-      <Header/>
-    </header>
+class App extends Component {
+  render() {
+    const { dispatch, isAuthenticated, errorMessage } = this.props
+    return (
+      <div>
+      <header>
+        <Header isAuthenticated={isAuthenticated} errorMessage={errorMessage} dispatch={dispatch}/>
+      </header>
 
-    <main>
-      <Route exact path="/" component={Dashboard} />
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/profile" component={Profile}/>
-    </main>
-  </div>
-);
+      <main>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/profile" component={Profile}/>
+      </main>
+    </div>
+  );
+  }
+}
 
-export default App;
+function mapStateToProps(state) {
+
+    const {auth} = state;
+    const {isAuthenticated, errorMessage} = auth;
+
+    return {
+        isAuthenticated,
+        errorMessage
+    }
+}
+
+export default connect(mapStateToProps)(App)
