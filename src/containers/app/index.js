9a94163f77aa2c570fa1884216a133ff26882a44
Tomas Hernandez <tohernandez@csumb.edu>
2018-03-29 09:04:19 -0700

    Reverse

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+
 import Header from '../../components/header';
 import Dashboard from '../dashboard';
 import Login from '../login';
@@ -8,11 +8,10 @@ import Profile from '../profile';
 
 class App extends Component {
   render() {
-    const { dispatch, isAuthenticated, errorMessage } = this.props
     return (
       <div>
       <header>
-        <Header isAuthenticated={isAuthenticated} errorMessage={errorMessage} dispatch={dispatch}/>
+        <Header/>
       </header>
 
       <main>
@@ -25,15 +24,4 @@ class App extends Component {
   }
 }
 
-function mapStateToProps(state) {
-
-    const {auth} = state;
-    const {isAuthenticated, errorMessage} = auth;
-
-    return {
-        isAuthenticated,
-        errorMessage
-    }
-}
-
-export default connect(mapStateToProps)(App)
+export default App;
