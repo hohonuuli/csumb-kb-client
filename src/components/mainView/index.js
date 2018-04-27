5c24f25d3df6dd8f3ae62bda55bd18f71eb3523d
Tomas Hernandez <tohernandez@csumb.edu>
2018-04-26 20:20:53 -0700

    Fixed session bug on refresh, removed package that wasn't needed

--- src/components/mainView/index.js
+++ src/components/mainView/index.js
@@ -3,6 +3,8 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 import { refreshConcept } from '../../actions/index';
+import { receiveLogin } from '../../actions/userActions';
+
 import './mainView.css';
 
 import TabView from '../tabView';
@@ -10,6 +12,9 @@ import TabView from '../tabView';
 class MainView extends Component {
   componentWillMount(){
     this.props.refreshConcept();
+    if(sessionStorage.getItem('access_token')){
+      this.props.receiveLogin(sessionStorage.getItem('access_token'));
+    }
   }
 
   render() {
@@ -31,7 +36,7 @@ function mapStateToProps(state) {
 }
 // Get actions and pass them as props to component
 function matchDispatchToProps(dispatch){
-    return bindActionCreators({refreshConcept: refreshConcept}, dispatch);
+    return bindActionCreators({refreshConcept: refreshConcept, receiveLogin: receiveLogin}, dispatch);
 }
 
 export default connect(mapStateToProps, matchDispatchToProps)(MainView);
